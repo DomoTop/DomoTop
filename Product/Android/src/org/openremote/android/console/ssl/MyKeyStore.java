17346ae2ae3e6e64255447064525b480c97fd2f8
Vincent Kriek <vincent@vincentkriek.nl>
2012-03-19 15:36:50 +0000

    Added MyKeyStore, it can generate a KeyPair and serialize it

---
+++ Product/Android/src/org/openremote/android/console/ssl/MyKeyStore.java
@@ -0,0 +1,104 @@
+package org.openremote.android.console.ssl;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+
+import org.openremote.android.console.Constants;
+
+import android.content.Context;
+import android.util.Log;
+
+public class MyKeyStore {
+
+	// Constants ------------------------------------------------------------------------------------
+	public final static String LOG_CATEGORY = Constants.LOG_CATEGORY + MyKeyStore.class.getName();
+	
+	private static final String KEYPAIR_FILE = "keypair";
+	private static final int KEYPAIR_SIZE = 2048;
+	private static final String KEYPAIR_ALGORITHM = "RSA";
+	
+	/**
+	 * Generates a KeyPair to be used in a certificate, this KeyPair is generated the first time 
+	 * and then serialized. It will return the just one KeyPair, which is never deleted.
+	 * @param context The current application context
+	 * @return A KeyPair 
+	 */
+	public KeyPair getKeyPair(Context context)
+	{
+		KeyPairGenerator keyGen = null;
+		try {
+			keyGen = KeyPairGenerator.getInstance(KEYPAIR_ALGORITHM);
+		} catch (NoSuchAlgorithmException e) {
+			Log.e(LOG_CATEGORY, "KeyPairGenerator could not be initialized with algorithm: " + KEYPAIR_ALGORITHM);
+		}
+		
+		keyGen.initialize(KEYPAIR_SIZE);
+
+		KeyPair keypair = deserializeKeypair(context);
+		if(keypair == null)
+		{
+			keypair = keyGen.generateKeyPair(); 
+			serializeKeypair(context, keypair);
+		}
+		
+		return keypair;
+	}
+	
+	/**
+	 * Serializes a KeyPair to the file specified in KEYPAIR_FILE
+	 * @param context The current application context
+	 * @param keypair The KeyPair that has to be serialized
+	 */
+	private static void serializeKeypair(Context context, KeyPair keypair) 
+	{	
+		try { 
+			File dir = context.getFilesDir();
+			File file = new File(dir, KEYPAIR_FILE);
+			
+			if(file != null)
+			{
+				boolean deleted = file.delete();
+				Log.d(LOG_CATEGORY, "Keypair file deleted: " + deleted);
+			}
+			
+			FileOutputStream output = context.openFileOutput(KEYPAIR_FILE, Context.MODE_PRIVATE);
+
+			ObjectOutput out = new ObjectOutputStream(output); 
+			out.writeObject(keypair); 
+			out.close(); 
+	 
+	    } catch(IOException ioe) { 
+	    	Log.e(LOG_CATEGORY, "Error writing keypair to file: ", ioe); 
+	    }
+	
+	}
+	
+	/**
+	 * Read a serialized KeyPair from the filesystem
+	 * @param context The current application context
+	 * @return The "de"serialized KeyPair
+	 */
+	private static KeyPair deserializeKeypair(Context context) {
+		KeyPair keypair = null;
+		try {
+			FileInputStream input = context.openFileInput(KEYPAIR_FILE);
+			ObjectInputStream in = new ObjectInputStream(input);
+			
+			keypair = (KeyPair) in.readObject();
+		} catch (IOException e) {
+			Log.e(LOG_CATEGORY, e.getMessage());
+		} catch (ClassNotFoundException e) {
+			Log.e(LOG_CATEGORY, e.getMessage());
+		}
+		
+		return keypair;
+	}
+}
