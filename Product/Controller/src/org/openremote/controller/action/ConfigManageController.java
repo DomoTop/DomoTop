055dbf4c394ad6d9edae3ec31e5a0e3f7a519818
Vincent Kriek <vincent@vincentkriek.nl>
2012-04-04 09:04:23 +0100

    Automatic sync when login

--- Product/Controller/src/org/openremote/controller/action/ConfigManageController.java
+++ Product/Controller/src/org/openremote/controller/action/ConfigManageController.java
@@ -161,6 +161,10 @@ public class ConfigManageController extends MultiActionController {
       return null;
    }
 
+   /**
+    * Save the username in the configurationtable
+    * @param username The username you want to save
+    */
    private void saveUsername(String username) {
       configurationService.addItem("composer_username", username);
    }
